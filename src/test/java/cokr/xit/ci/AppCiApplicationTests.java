a87653a8571484fe89194254f4a03fde772bca18
시온파파 <minkyu1128@gmail.com>
2022-03-06 10:28:00 +0900

    최초 커밋

---
+++ src/test/java/cokr/xit/ci/AppCiApplicationTests.java
@@ -0,0 +1,13 @@
+package cokr.xit.ci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppCiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
