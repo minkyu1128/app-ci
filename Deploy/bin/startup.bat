6e4555416feca5fed8e2c7afacd62afde03df8d8
시온파파 <minkyu1128@gmail.com>
2022-03-07 01:51:19 +0900

    f

--- Deploy/bin/startup.bat
+++ Deploy/bin/startup.bat
@@ -5,4 +5,4 @@ rem java -jar ..\webapp\ci-1.0.0-SNAPSHOT.war
 
 rem Started background...
 @START /b shutdown.bat
-@START /b C:\xit\spring-tool-suite-4-4.11.0.RELEASE-e4.20.0-win32.win32.x86_64.self-extracting\env-setting\Java\jdk1.8.0_121\bin\java  "-Dspring.profiles.active=abc" -jar ..\webapp\ci-1.0.0-SNAPSHOT.war
+@START /b C:\xit\spring-tool-suite-4-4.11.0.RELEASE-e4.20.0-win32.win32.x86_64.self-extracting\env-setting\Java\jdk1.8.0_121\bin\java  "-Dspring.profiles.active=prod" -jar ..\webapp\ci-1.0.0-SNAPSHOT.war
