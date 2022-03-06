58ec62c24ccddc53abcfa42e60a48fe19148dbae
시온파파 <minkyu1128@gmail.com>
2022-03-07 01:42:29 +0900

    yaml 파일 local/prod 분리

--- Deploy/bin/startup.bat
+++ Deploy/bin/startup.bat
@@ -5,4 +5,4 @@ rem java -jar ..\webapp\ci-1.0.0-SNAPSHOT.war
 
 rem Started background...
 @START /b shutdown.bat
-@START /b .\Java\jdk1.8.0_121\bin\java -jar ..\webapp\ci-1.0.0-SNAPSHOT.war -Dspring.profiles.active=prod
+@START /b .\Java\jdk1.8.0_121\bin\java "-Dspring.profiles.active=prod" -jar ..\webapp\ci-1.0.0-SNAPSHOT.war
